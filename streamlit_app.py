bb60fc6fd34c3875abbb656bab44f3a69653d9a9
69darkfrog69 <69darkfrog69@gmail.com>
2026-04-05 01:31:10 +0000

    Change connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-
-session = get_active_session()
+cnx = st.connection('snowflake')
+session = cnx.session()
 st.title(":cup_with_straw: Cutomize Your Smoothie!:cup_with_straw:")
 st.write(
   """Choose the fruit you want in your custom Smoothie!
